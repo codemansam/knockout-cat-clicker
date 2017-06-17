ea59761b6574dad7692de271d12a99240f772011
codemansam <sam@stacked.co.nz>
2017-06-17 15:39:12 +1200

    Levels binding success

--- js/app.js
+++ js/app.js
@@ -3,17 +3,17 @@ var ViewModel = function() {
 	this.name = ko.observable('Tabby');
 	this.imgSrc = ko.observable('img/tabby.jpg')
 	this.imgAttribution = ko.observable('https://www.flickr.com/photos/big')
-	this.nickName = ko.observable('Newborn');
+	this.level = ko.observable('Newborn');
 
 	this.incrementCounter = function () {
+
 	this.clickCount(this.clickCount() +1);
-	};
-
-	this.changeNickName = ko.computed(function() {
-		if (this.clickCount <= 10) {
-			return this.nickName('Infant');
+		if (this.clickCount() > 10) {
+			this.level('Mature');
 		}
-	}, this);
-}
+	}
 
-ko.applyBindings(new ViewModel())
+
+};
+
+ko.applyBindings(new ViewModel());
